7112ed89c590990c8bc0b4903dcc284f3b447c72
Tarikul Islam <tarikul3639@gmail.com>
2026-07-11 18:53:54 +0600

    Project setup

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,19 @@
+
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('AppController', () => {
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [],
+      providers: [],
+    }).compile();
+
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+  
+    });
+  });
+});
